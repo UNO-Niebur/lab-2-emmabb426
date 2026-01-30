9e7ad534d73eace3db391b44fd20b94dff83dc2e
emmabb426 <ebarnes17@unomaha.edu>
2026-01-30 19:49:43 +0000

    End of Magic 8 Ball

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,18 +1,24 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Emma Barnes
+#Date: 01/30/2026
+#Assignment: Lab 02 - Magic 8 Ball
 
 #We will need random for this program, import to use this package.
 import random
 
 def main():
   #Create a list of your responses.
+  answers = ["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now."
+             "Concentrate and ask again.", "Don't count on it.", "It is certain.", 
+             "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.",
+             "Outlook not so good.", "Outlook good.", "Reply hazy, try again.",
+             "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.",
+             "Yes - definitely.", "You may rely on it."]
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  input("Ask me your deepest question: ")
   #Answer question randomly with one of the options from your earlier list.
-
+  print(random.choice(answers))
 
 if __name__ == '__main__':
   main()
